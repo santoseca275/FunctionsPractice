3384618d3a370b09ef3ef961fe96f2a423880a67
Erick C. Allen, Sr <eca275@gmail.com>
2017-01-31 21:27:15 -0500

    Homework

---
+++ main-context.js
@@ -0,0 +1,43 @@
+// Excercise 1
+// Q1
+function sum (x,y) {
+
+  var result = x + y;
+  return result;
+}
+
+
+// Q2
+function avg(a,b,c) {
+
+  var result = (a + b + c) / 3;
+  return result;
+}
+
+// Q3
+function getLength(y) {
+  var result = y.length;
+  return result;
+}
+
+// Q4
+function greaterThan(y,z) {
+  return result = y > z;
+  }
+
+// Q5
+function greet(x) {
+  var result = "Hello" +  " " + x + '!';
+  return result;
+}
+
+// Q6
+function madlib(w, x, y, z) {
+  var result = w + ' ' + x + ' ' + y + ' ' + z;
+  return result ;
+  }
+
+
+  // Excercise 2
+  // Q1
+  
